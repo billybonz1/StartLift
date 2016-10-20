04fd9f6f88289c899692b05fc666786d3e4e7aa1
jeka <ElectroDeggi33759@yandex.ru>
2016-10-20 18:17:24 +0300

    dfs22

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,9 @@
 $(function() {
 
 	// Custom JS
+    $('.open-popup-link').magnificPopup({
+        type:'inline',
+        midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });
 
 });
